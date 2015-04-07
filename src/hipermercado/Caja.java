bb4b54e461355e92d126b7f717d92ca0d227f389
Laura <l4depende@gmail.com>
2015-04-07 14:17:58 +0100

    Update Caja.java

--- src/hipermercado/Caja.java
+++ src/hipermercado/Caja.java
@@ -1,13 +1,43 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hipermercado;
 
-/**
- *
- * @author usuario
- */
-public class Caja {
+public class Caja extends Thread {
     
+    private final Cola cola;
+    private final Contabilidad cajaFuerte;
+    private double recaudacion;
+    
+    public Caja(Cola cola, Contabilidad cajaFuerte) {
+        this.cola = cola;
+        this.cajaFuerte = cajaFuerte;
+    }
+    
+    public void run(){
+    
+        try{
+            /*
+             mientras haya clientes en la cola:
+             *  atendercliente (dormir el valor del carro/10);
+             */
+            Cliente cliente = cola.sacar();
+            while(cliente != null){
+                double recaudacionParcial = cliente.damePrecioCarro();
+                this.recaudacion += recaudacionParcial;
+                Thread.sleep((long)recaudacionParcial/10);
+            }
+            
+        }
+        catch(Exception e){
+         //Me han interrumpido y.y
+        }
+        finally{
+            pasarLaContabilidad();
+        }
+    }
+
+    private void pasarLaContabilidad() {
+        cajaFuerte.añadeSaldo(recaudacion);
+    }
+
+        
 }
