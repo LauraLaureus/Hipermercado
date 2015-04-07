30c26bbaad538058c17ea1108e0018a222e231ee
Laura <l4depende@gmail.com>
2015-04-07 14:18:19 +0100

    Update Cola.java

--- src/hipermercado/Cola.java
+++ src/hipermercado/Cola.java
@@ -1,14 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hipermercado;
 
-/**
- *
- * @author usuario
- */
+
 public class Cola {
+
+    private int tamaño;
+    private ClienteEnCola porElPrincipio;
+    private ClienteEnCola porElFinal;
     
-    private String laura = "es mu sersi";
+    private class ClienteEnCola{
+        public Cliente cl;
+        public ClienteEnCola siguiente;
+    }
+    
+    public synchronized Cliente sacar() {
+        /*Saca y extrae si no hay nada devolver null*/
+    }
+    
+    public synchronized void añadirFinal(Cliente cl){}
+    public synchronized void añadirPrincipio(Cliente cl){}
+    
+    public int tamañoMaximo(){}
+    
+    private void modificaElTamañoMaximo(int nuevo){}
 }
